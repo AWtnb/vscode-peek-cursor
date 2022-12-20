f8301aa69cd59777a311ecca6f29d93d87119e32
awtnb <arikaebanataw@gmail.com>
2022-12-21 00:21:47 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# peek-cursor README
+
+Peek cursor or selection out of visible area. Useful when executed after <kbd>ctrl+shift+l</kbd>.
+
+## Features
+
++ `peek-cursor.peekNext` : Scroll DOWN to the first cursor out-of-screen.
++ `peek-cursor.peekPrevious` : Scroll UP to the first cursor out-of-screen.
+
+
+**Enjoy!**
+
+------------------------------
+
+アイコン画像：[「いろいろな目の表情のイラスト」 by いらすとや](https://www.irasutoya.com/2015/07/blog-post_500.html)
---
+++ src/extension.ts
@@ -0,0 +1,60 @@
+import * as vscode from "vscode";
+
+const sortSelections = (editor: vscode.TextEditor): vscode.Selection[] => {
+  return editor.selections.slice().sort((a: vscode.Selection, b: vscode.Selection): number => {
+    if (a.start.line < b.start.line) {
+      return -1;
+    }
+    if (b.start.line < a.start.line) {
+      return 1;
+    }
+    return 0;
+  });
+};
+
+const peekNext = (editor: vscode.TextEditor) => {
+  if (editor.selections.length < 2) {
+    return;
+  }
+  const sorted = sortSelections(editor);
+  const visible = editor.visibleRanges[0];
+  const unseenCursors = sorted.filter((sel) => {
+    return visible.end.line < sel.start.line;
+  });
+  const targetLine = unseenCursors.length < 1 ? sorted[0].start.line : unseenCursors[0].start.line;
+  vscode.commands.executeCommand("revealLine", {
+    lineNumber: targetLine,
+    at: "top",
+  });
+};
+
+const peekPrevious = (editor: vscode.TextEditor) => {
+  if (editor.selections.length < 2) {
+    return;
+  }
+  const sorted = sortSelections(editor);
+  const visible = editor.visibleRanges[0];
+  const pastCursors = sorted.filter((sel) => {
+    return sel.end.line < visible.start.line;
+  });
+  const targetLine = pastCursors.length < 1 ? sorted[sorted.length - 1].end.line : pastCursors[pastCursors.length - 1].start.line;
+  vscode.commands.executeCommand("revealLine", {
+    lineNumber: targetLine,
+    at: "top",
+  });
+};
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand("peek-cursor.peekNext", (editor: vscode.TextEditor) => {
+      peekNext(editor);
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand("peek-cursor.peekPrevious", (editor: vscode.TextEditor) => {
+      peekPrevious(editor);
+    })
+  );
+}
+
+export function deactivate() {}
